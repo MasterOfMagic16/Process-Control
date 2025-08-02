e90f606f5db2ce17026f632a346e745f41060b3f
MasterOfMagic16 <brmarcy2021@gmail.com>
2025-08-01 22:17:17 -0400

    GUI Looks pretty good. Should add that it scales as the window is dragged.

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -5,7 +5,9 @@ from main import Main
 # Root
 root = tk.Tk()
 root.title('Python Process Control Simulator')
-root.geometry('620x450')
+root.geometry('620x280')
+
+#TODO: Make entry boxes smaller, not too small.
 
 # Variable Tracker
 guiVars = GUIVars()
@@ -33,10 +35,10 @@ def UpdateProcessConfigFrame():
 # Process Config Frame
 processConfigFrame = tk.LabelFrame(root, text="Process", width=300, height=150, borderwidth=2, relief='groove')
 processConfigFrame.grid_propagate(False)
-processConfigFrame.grid(row=0, column=0, padx=5, sticky='nsew')
+processConfigFrame.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
 
 # Process Select Frame
-processSelectFrame = tk.Frame(processConfigFrame, width=100, height=120, borderwidth=2, relief='groove')
+processSelectFrame = tk.Frame(processConfigFrame, width=100, height=125, borderwidth=2, relief='groove')
 processSelectFrame.pack_propagate(False)
 processSelectFrame.grid(row=0, column=0, padx=5, sticky='nsew')
 tk.Label(processSelectFrame, text='Process Type:').pack(anchor='w')
@@ -46,10 +48,10 @@ tk.Radiobutton(processSelectFrame, text="Second Order", variable=guiVars.process
 tk.Checkbutton(processSelectFrame, text='DeadTime?', variable=guiVars.deadTimeType, command=UpdateProcessConfigFrame).pack(anchor='w')
 
 # Process Parameter Frame
-processParameterFrame = tk.Frame(processConfigFrame, width=175, borderwidth=2, relief='groove')
+processParameterFrame = tk.Frame(processConfigFrame, width=180, borderwidth=2, relief='groove')
 processParameterFrame.pack_propagate(False)
 processParameterFrame.grid(row=0, column=1, sticky='nsew')
-tk.Label(processParameterFrame, text='Process Parameters:').pack(anchor='e')
+tk.Label(processParameterFrame, text='Process Parameters:').pack(anchor='e', pady=1)
 
 KpParamFrame = tk.Frame(processParameterFrame)
 tk.Label(KpParamFrame, text='Kp:').grid(row=0, column=0)
@@ -81,32 +83,32 @@ def UpdateControlConfigFrame():
     TdParamFrame.pack_forget()
 
     if guiVars.pControl.get() or guiVars.iControl.get() or guiVars.dControl.get():
-        KcParamFrame.pack(anchor='e')
+        KcParamFrame.pack(anchor='e', pady=2)
     if guiVars.iControl.get():
-        TiParamFrame.pack(anchor='e')
+        TiParamFrame.pack(anchor='e', pady=2)
     if guiVars.dControl.get():
-        TdParamFrame.pack(anchor='e')
+        TdParamFrame.pack(anchor='e', pady=2)
 
 
 # Controller Config Frame
 controlConfigFrame = tk.LabelFrame(root, text="Controller", width=300, height=150, borderwidth=2, relief='groove')
 controlConfigFrame.grid_propagate(False)
-controlConfigFrame.grid(row=0, column=1, padx=5, sticky='nsew')
+controlConfigFrame.grid(row=0, column=1, padx=5, pady=5, sticky='nsew')
 
 # Controller Select Frame
-controlSelectFrame = tk.Frame(controlConfigFrame, width=150)
-controlSelectFrame.grid_propagate(False)
-controlSelectFrame.grid(row=0, column=0)
+controlSelectFrame = tk.Frame(controlConfigFrame, width=100, height=125, borderwidth=2, relief='groove')
+controlSelectFrame.pack_propagate(False)
+controlSelectFrame.grid(row=0, column=0, padx=5, sticky='nsew')
 tk.Label(controlSelectFrame, text='Controller Type:').pack()
 tk.Checkbutton(controlSelectFrame, text='Proportional', variable=guiVars.pControl, command=UpdateControlConfigFrame).pack(anchor='w')
 tk.Checkbutton(controlSelectFrame, text='Integral', variable=guiVars.iControl, command=UpdateControlConfigFrame).pack(anchor='w')
 tk.Checkbutton(controlSelectFrame, text='Derivative', variable=guiVars.dControl, command=UpdateControlConfigFrame).pack(anchor='w')
 
 # Controller Parameter Frame
-controlParameterFrame = tk.Frame(controlConfigFrame, width=150)
-controlParameterFrame.grid_propagate(False)
+controlParameterFrame = tk.Frame(controlConfigFrame, width=185, height=125, borderwidth=2, relief='groove')
+controlParameterFrame.pack_propagate(False)
 controlParameterFrame.grid(row=0, column=1, sticky='nsew')
-tk.Label(controlParameterFrame, text='Controller Parameters:').pack(anchor='e')
+tk.Label(controlParameterFrame, text='Controller Parameters:').pack(anchor='e', pady=1)
 
 KcParamFrame = tk.Frame(controlParameterFrame)
 tk.Label(KcParamFrame, text='Kc:').grid(row=0, column=0, sticky='e')
@@ -124,37 +126,42 @@ UpdateControlConfigFrame()
 
 
 # Disturbance Frame
-DisturbanceFrame = tk.LabelFrame(root, text="Disturbance", borderwidth=2, relief='groove')
-DisturbanceFrame.grid(row=1, column=0, sticky='nsew')
+DisturbanceFrame = tk.LabelFrame(root, width=300, height=75, text="Disturbance", borderwidth=2, relief='groove')
+DisturbanceFrame.grid_propagate(False)
+DisturbanceFrame.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
 
-DistStepParamFrame = tk.Frame(DisturbanceFrame)
-DistStepParamFrame.grid(row=0, column=0)
-tk.Label(DistStepParamFrame, text='Disturbance Step Change: ').grid(row=0, column=0)
+DistStepParamFrame = tk.Frame(DisturbanceFrame, width=130, height=50, borderwidth=2, relief='groove')
+DistStepParamFrame.grid_propagate(False)
+DistStepParamFrame.grid(row=0, column=0, padx=5)
+tk.Label(DistStepParamFrame, text='Step Change: ').grid(row=0, column=0)
 tk.Entry(DistStepParamFrame, textvariable=guiVars.DistStepParam, justify='center').grid(row=1, column=0)
 
-KdParamFrame = tk.Frame(DisturbanceFrame)
-KdParamFrame.grid(row=0, column=1)
+KdParamFrame = tk.Frame(DisturbanceFrame, width=130, height=50, borderwidth=2, relief='groove')
+KdParamFrame.grid_propagate(False)
+KdParamFrame.grid(row=0, column=1, padx=15, sticky='e')
 tk.Label(KdParamFrame, text='Kd:').grid(row=0, column=0)
 tk.Entry(KdParamFrame, textvariable=guiVars.KdParam, justify='center').grid(row=1, column=0)
 
 
 # SetPoint Frame
-SetPointFrame = tk.LabelFrame(root, text="Set Point", width=250, borderwidth=2, relief='groove')
-SetPointFrame.grid(row=1, column=1, columnspan=4, sticky='nsew')
+SetPointFrame = tk.LabelFrame(root, text="Set Point", width=300, height=75, borderwidth=2, relief='groove')
+SetPointFrame.grid_propagate(False)
+SetPointFrame.grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
 
-SPStepParamFrame = tk.Frame(SetPointFrame)
-SPStepParamFrame.grid(row=0)
-tk.Label(SPStepParamFrame, text='SetPoint Step Change:').grid(row=0, column=0, sticky='n')
+SPStepParamFrame = tk.Frame(SetPointFrame, width=130, height=50, borderwidth=2, relief='groove')
+SPStepParamFrame.grid_propagate(False)
+SPStepParamFrame.grid(row=0, padx=85)
+tk.Label(SPStepParamFrame, text='Step Change:').grid(row=0, column=0)
 tk.Entry(SPStepParamFrame, textvariable=guiVars.SPStepParam, justify='center').grid(row=1)
 
 
 # GO Button
-def GO():
+def RunSimulation():
     Main(guiVars.GetData())
 
 
-activateButton = tk.Button(root, text='GO', command=GO)
-activateButton.grid(row=2, columnspan=2, sticky='nsew')
+activateButton = tk.Button(root, text='Run Simulation', command=RunSimulation)
+activateButton.grid(row=2, columnspan=2, padx=5, pady=5, sticky='nsew')
 
 
 # Loop Root
